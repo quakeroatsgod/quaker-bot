8d9b27974822418b891c9fb600d19a724c673a4a
quakeroatsgod <agnellogriffen@gmail.com>
2022-06-23 22:02:54 -0700

    Bot can ping my friend matt

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // Require the necessary discord.js classes
 const { Client, Intents, Interaction } = require('discord.js');
-const { token } = require('./config.json');
+const { token,clientId,pingMattChannelId,mattId } = require('./config.json');
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log(`Bot app is running as ${client.user.tag}`);
 });
+
 //Commands using "/command"
 client.on('interactionCreate', async interaction => {
     console.log(interaction.user.id)
@@ -19,14 +20,17 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.on('message', (message) => {
-    console.log('aaff')
-    if(message.user.id!="142381065485418496"){
-        message.reply('aa!');
-    }
+//Event handler when a message is sent
+client.on('messageCreate', async (message) => {
+    console.log(`Message sent by ${message.author.tag}. Content <${message.content}>`);
+    //Base case, bot shouldn't worry about its own message
+    if(message.author.id===clientId) return;
+    //If the message was sent in #ping-matt-here
+    if(message.channel.id===pingMattChannelId)  return message.channel.send(`<@${mattId}>`)
 });
 
 
 
 // Login to Discord with your client's token
 client.login(token);
+
